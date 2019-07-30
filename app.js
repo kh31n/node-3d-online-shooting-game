c6c3c1cf155be870f481085d5eef792027265893
hirotoshi kawai <eternalxaion2@gmail.com>
2019-07-30 20:50:38 +0900

    camera position fixed.

--- app.js
+++ app.js
@@ -7,8 +7,12 @@
 const express = require('express');
 const app = express()
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render("./index.ejs");
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("server starting...\nhttp:localhost:3000 can see."));
